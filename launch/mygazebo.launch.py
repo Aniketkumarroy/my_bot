637fbdefb85df845038c76cee1f63dfa3275bee9
Aniket Kumar Roy <aniketroy769@gmail.com>
2024-06-11 09:57:36 +0000

    gazebo can launch custom world now

--- launch/mygazebo.launch.py
+++ launch/mygazebo.launch.py
@@ -1,51 +1,46 @@
-import os
 
-from ament_index_python.packages import get_package_share_directory
-
+"""Launch Gazebo server and client with command line arguments."""
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-
-from launch_ros.actions import Node
-
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
-import xacro
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
 
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
 
+    package_name = "my_bot"
 
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
+    wodrl_param = DeclareLaunchArgument('world', default_value='',
+                          description='gazebo World')
 
-    package_name='my_bot' #<--- CHANGE ME
-
-    rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
-    )
-
-    # Include the Gazebo launch file, provided by the gazebo_ros package
-    gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
-
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'kuchh_bhi'],
-                        output='screen')
-
-
-
-    # Launch them all!
     return LaunchDescription([
-        rsp,
-        gazebo,
-        spawn_entity
+        DeclareLaunchArgument('gui', default_value='true',
+                              description='Set to "false" to run headless.'),
+
+        DeclareLaunchArgument('server', default_value='true',
+                              description='Set to "false" not to run gzserver.'),
+
+        wodrl_param,
+
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gzserver.launch.py')]),
+            condition=IfCondition(LaunchConfiguration('server')),
+            launch_arguments=[('world', LaunchConfiguration('world'))]
+
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gzclient.launch.py')]),
+            condition=IfCondition(LaunchConfiguration('gui'))
+        ),
     ])
